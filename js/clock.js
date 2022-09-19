02fdcab59411e001f5361090b5e73c26c34ba902
tahakoner <tahakoner@gmail.com>
2022-09-19 17:51:42 +0300

    new

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,21 @@
 let userName= prompt("Kullanıcı adı giriniz: ")
 let info= document.querySelector("#myName")
 info.innerHTML= `${userName}`
+
+function tarihSaat(){
+    var date=  new Date().toLocaleString('tr-TR');
+    document.getElementById('myClock').innerHTML= date;
+}
+
+setInterval(tarihSaat)
+
+
+
+
+
+
+
+
+
+
+
